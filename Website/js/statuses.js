13a3d2456829f7d9b4b0dd41551bc6c46a551b0b
Greg O'Connor <gregory@oconnor.name>
2014-02-22 19:17:16 +0000

    Initial attempt at processing statuses

--- Website/js/statuses.js
+++ Website/js/statuses.js
@@ -1,5 +1,25 @@
-function testStatuses() {
-  FB.api('/me/statuses/', function(response) {
-    console.log(response)
+function processStatuses() {
+  var nameToLikeCount = {};
+
+  FB.api('/me/statuses/?limit=50', function(response) {
+    console.log("Processing status response");
+    var data = response.data;
+    _.each(data, function(status) {
+      var likes = status.likes;
+      if (typeof likes != "undefined") {
+        _.each(likes.data, function(like) {
+          var count = nameToLikeCount[like.id];
+          if (typeof count != "undefined") {
+            nameToLikeCount[like.id] = count + 1;
+          } else {
+            nameToLikeCount[like.id] = 1;
+          }
+        });
+      }
+    });
+
+    console.log(nameToLikeCount);
+
+    var next = response.paging.next;
   });
 }
