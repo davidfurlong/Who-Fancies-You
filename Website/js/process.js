8e97c4aa7d5b39e45cc27dc9e86a0b5be67a5482
James Thompson <jmt158@gmail.com>
2014-02-22 16:46:55 +0000

    Status branch

---
+++ Website/js/process.js
@@ -0,0 +1,8 @@
+// Here we run a very simple test of the Graph API after login is successful. 
+// This testAPI() function is only called in those cases. 
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me/posts/', function(response) {
+    console.log(response)
+  });
+}
