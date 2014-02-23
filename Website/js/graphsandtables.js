49713ed960ba3a4e569b9a27091b024a6b81ae7f
David Furlong <dvfurlong@googlemail.com>
2014-02-23 04:50:46 +0000

    Graphs + Tables UI

--- Website/js/graphsandtables.js
+++ Website/js/graphsandtables.js
@@ -5,23 +5,73 @@ function makeLoveTable(result){
 	var t = 0;
 	if(i>0) t = result[0].score;
 
-	for(var iter=0;iter<5;iter++){
-		if(iter==0 || result[iter].score>t/2){
+	for(var iter=0;iter<8;iter++){
+		if(iter==0 || result[iter].score>0.10){
 			var prob = "";
 			var v = result[iter].score;
-			if(v>0.2) prob = "Very Likely";
+			if(v>0.9) prob = "Obsessed";
+			else if(v>0.2) prob = "Very Likely";
 			else if(v>0.15) prob = "Likely";
 			else if(v>0.10) prob = "Perhaps";
 			else prob = "Unlikely";
 
-			
-			
-			$('#loveTable table').append('<tr><td><span height="auto" width="auto" class="person-pic-wrapper"><img class="person-pic" src="http://graph.facebook.com/'+result[iter].id+'/picture?type=square&width=200&height=200"/></span>'+result[iter].name+'</td><td>'+prob+'</td></tr>');	
+
+			//result[iter].name.length;
+
+			$('#loveTable table').append('<tr><td><img class="person-pic" src="http://graph.facebook.com/'+result[iter].id+'/picture?type=square&width=200&height=200"/></td><td>'+result[iter].name+'</td><td>'+prob+'</td></tr>');	
 		}
 	}
 }
 function makeLoveGraph(result){
-	//$('#loveGraph')
+
+	var d = [];
+	for(var i=0;i<result.length;i++){
+		if(isNaN(result[i].score))
+			d.push((0).toFixed(3));
+		else
+			d.push((100*result[i].score).toFixed(3));
+	}
+	d = d.reverse();
+
+	var dnew = [0,0,0,0,0,0,0,0,0,0];
+	var j = 0;
+	var k = 0;
+	var c = 0;
+	while(j<d.length){
+		k = j;
+
+		while(c*10<=parseInt(d[k]) && parseInt(d[k])<((c*10)+10) && k<d.length){
+			dnew[c]=dnew[c]+1;
+			k++;
+		}
+		c++;
+		j = k;
+		j++;
+	}
+
+	var w = $('#loveGraph').width();
+	var newG = $.parseHTML('<h4>All your friends</h4><canvas id="myChart" width="'+w+'" height="260"></canvas>');
+	$('#loveGraph').append(newG);
+	//var ctx = newG.get(0).getContext("2d");
+	//Get context with jQuery - using jQuery's .get() method.
+	var ctx = $("#myChart").get(0).getContext("2d");
+	//ctx.fillText("Freq",0,w-20);
+	//This will get the first returned node in the jQuery collection.
+	var myNewChart = new Chart(ctx);
+	var data = {
+		labels : ["0","10","20","30","40","50","60","70","80","90","100"],
+		datasets : [
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				data : dnew
+			}
+		]
+	}
+	new Chart(ctx).Line(data,{bezierCurve:false,pointDot:false});
+
 }
 function makeHateTable(result){
 	//$('#hateTable')
