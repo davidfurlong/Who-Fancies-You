154a3d0e95e07a9b47660e7f1326fbd66e1d691d
Greg O'Connor <gregory@oconnor.name>
2014-02-23 03:13:38 +0000

    Remove debug prints

--- Website/js/process.js
+++ Website/js/process.js
@@ -20,16 +20,19 @@ function whoLikesMe(callback) {
   processStatuses(function(result) {
     results.push({map: result, weight: STATUSES_WEIGHT});
     console.log("Statuses returned");
+    console.log(result["633530715"]);
     latch.complete();
   });
   processPhotos(function(result) {
     results.push({map: result, weight: PHOTOS_WEIGHT});
     console.log("Photos returned");
+    console.log(result["633530715"]);
     latch.complete();
   });
   calculateMessageScore(function(result, certainty) {
     results.push({map: result, weight: MESSAGES_WEIGHT});
     console.log("Messages returned");
+    console.log(result["633530715"]);
     latch.complete();
   });
 }
@@ -38,7 +41,7 @@ function mergeCollections(c1, c2, weight) {
   var result = c1;
   _.each(c2, function(value, key) {
     if (typeof c1[key] != "undefined") {
-      result[key].score = c1[key] + value.score * weight;
+      result[key].score = c1[key].score + value.score * weight;
     } else {
       result[key] = {name: value.name};
       result[key].score = value.score * weight;
