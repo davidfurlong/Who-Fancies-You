2b638c6a88077fcc4572e2a11f63214e66013587
David Furlong <dvfurlong@googlemail.com>
2014-02-23 01:36:01 +0000

    New data structure Fix

--- Website/js/graphsandtables.js
+++ Website/js/graphsandtables.js
@@ -3,17 +3,17 @@
 function makeLoveTable(result){
 	var i = result.length;
 	var t = 0;
-	if(i>0) t = result[0].value;
+	if(i>0) t = result[0].score;
 
 	for(var iter=0;iter<5;iter++){
-		if(iter==0 || result[iter].value>t/2){
+		if(iter==0 || result[iter].score>t/2){
 			var prob = "";
-			var v = result[iter].value;
+			var v = result[iter].score;
 			if(v>0.2) prob = "Very Likely";
 			else if(v>0.15) prob = "Likely";
 			else if(v>0.10) prob = "Perhaps";
 			else prob = "Unlikely";
-			$('#loveTable table').append('<tr><td><span height="auto" width="auto" class="person-pic-wrapper"><img class="person-pic" src="http://graph.facebook.com/'+result[iter].key+'/picture?type=square&width=200&height=200"/></span>'+prob+'</td><td></td></tr>');	
+			$('#loveTable table').append('<tr><td><span height="auto" width="auto" class="person-pic-wrapper"><img class="person-pic" src="http://graph.facebook.com/'+result[iter].id+'/picture?type=square&width=200&height=200"/></span>'+result[iter].name+'</td><td>'+prob+'</td></tr>');	
 		}
 	}
 }
